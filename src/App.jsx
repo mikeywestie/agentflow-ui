5103fc7db18c6728ea8ffdf1c98424f59fe4e47e
Mikey <mikeywestman@gmail.com>
2026-06-17 15:38:13 +0200

    feat: add dedicated dashboard routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage";
 import ExecutionDetailsPage from "./pages/ExecutionDetailsPage";
+import ExecutionsPage from "./pages/ExecutionsPage";
+import AgentsPage from "./pages/AgentsPage";
+import WorkflowsPage from "./pages/WorkflowsPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<DashboardPage />} />
+        <Route path="/executions" element={<ExecutionsPage />} />
         <Route path="/executions/:id" element={<ExecutionDetailsPage />} />
+        <Route path="/agents" element={<AgentsPage />} />
+        <Route path="/workflows" element={<WorkflowsPage />} />
       </Routes>
     </BrowserRouter>
   );
